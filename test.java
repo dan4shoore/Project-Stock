f37cc332bd82e01459f724b1a44593f4e0207096
Aappo001 <aappolon0321@gmail.com>
2024-04-08 19:18:12 -0400

    modified:   test.java 	modified:   testOutput.csv

--- test.java
+++ test.java
@@ -13,7 +13,7 @@ public class test{
         List<String[]> csvTest = csvData(csv1Data);
 
         for (String s: csvTest.get(0)) {
-            System.out.println(s);
+          //  System.out.println(s);
         }
 
         outputCSV(csv1Data, csv2Data);
@@ -31,53 +31,63 @@ public class test{
             csvName.add(list[1]);  
             csvQTY.add(list[2]);
         }
+        
         csvID.remove(0);
         csvName.remove(0);
         csvQTY.remove(0);
+        
         String[] csvIDFin = csvID.toArray(new String[0]);
         String[] csvNameFin = csvName.toArray(new String[0]);
         String[] csvQTYFin = csvQTY.toArray(new String[0]);
+        
         csvData.add(csvIDFin);
         csvData.add(csvNameFin);
         csvData.add(csvQTYFin);
 
         return csvData;
-
-
-
-
     }
 
 
     public static void outputCSV(List<String[]> file1, List<String[]> file2) {
         String filePath = "testOutput.csv";
-
-        List<String[]> outputCSV = null;
         List<String[]> csv1 = csvData(file1);
         List<String[]> csv2 = csvData(file2);
+        int maxArrayLength = 0;
+        if(csv1.get(0).length > csv2.get(0).length ) {
+            maxArrayLength = csv1.get(0).length;
+        }
+        else {
+            maxArrayLength = csv2.get(0).length;
+        }
+        List<Integer> commonID1 = new ArrayList<>();
+        List<Integer> commonID2 = new ArrayList<>();
+
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
 
-            writer.write("ID,Name,QTY1,QTY2,Diference");
+            writer.write("ID,Name,QTY1,QTY2,Difference");
             writer.newLine();
+            
+            for (int i = 0; i < csv1.get(0).length; i++) {
+                for(int j = 0; j < csv2.get(0).length; j++) {
+                    if (csv1.get(0)[i].equalsIgnoreCase(csv2.get(0)[j])) {
 
-        int count = 0;
-        
-        for (String s: csv1.get(0)) {
-            if (s.equalsIgnoreCase(csv2.get(0)[0])) {
-                System.out.println("This is true");
-               // writer.write();
-               writer.write(s + "," + csv1.get(1)[count] + "," + csv1.get(2)[count] + "," + csv2.get(2)[count] +  "," + "TRUE");
+                        commonID1.add(i);
+                        commonID2.add(j);
+                    }
+                }
+            }
+    
+            for (int i = 0; i < commonID1.size(); i++) {
+                String id = csv1.get(0)[commonID1.get(i)];
+                String name = csv1.get(1)[commonID1.get(i)];
+                String qty1 = csv1.get(2)[commonID1.get(i)];
+                String qty2 = csv2.get(2)[commonID2.get(i)];
+                writer.write(id + "," + name + "," + qty1 + "," + qty2 + "," + !qty1.equalsIgnoreCase(qty2));
                 writer.newLine();
             }
-            else {
-                System.out.println("This is false");
-                writer.write(s + "," + csv1.get(1)[count] + "," + csv1.get(2)[count] + "," + csv2.get(2)[count] + "," + "FALSE");
-                writer.newLine();
-            }
-            count++;
         }
-    }
+        
     catch (IOException e) {
         e.printStackTrace();
     }
@@ -95,5 +105,7 @@ public class test{
         }
         return csvData;
     }
+
+
     
 }
