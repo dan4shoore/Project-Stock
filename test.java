f21e72277afcaeb83a9a919ed680de4fbdcdd937
Your Name <Davidcas7@hotmail.com>
2024-04-13 11:34:38 -0400

    Implemented Abdias code, and testing procedures

--- test.java
+++ test.java
@@ -9,33 +9,32 @@ public class test{
 
         List<String[]> csv1Data = readCSV(file1);
         List<String[]> csv2Data = readCSV(file2);
-
-        List<String[]> csvTest = csvData(csv1Data);
-
-        for (String s: csvTest.get(0)) {
-          //  System.out.println(s);
-        }
-
+       
         outputCSV(csv1Data, csv2Data);
 
     }
 
+
+    // Grabs the IDs, names, and quantitys and organizes the data in a list
     public static List<String[]> csvData(List<String[]> csvFile) {
         List<String[]> csvData = new ArrayList<>();
         List<String> csvID = new ArrayList<>();
         List<String> csvName = new ArrayList<>();
         List<String> csvQTY = new ArrayList<>();
 
+        // Seperatews the IDs, names, and quantities into their own respective lists
         for (String[] list: csvFile) {
             csvID.add(list[0]);
             csvName.add(list[1]);  
             csvQTY.add(list[2]);
         }
         
+        // Removes the header so that only the data is being read
         csvID.remove(0);
         csvName.remove(0);
         csvQTY.remove(0);
         
+        // Convert each column list to an array and add them to main data list
         String[] csvIDFin = csvID.toArray(new String[0]);
         String[] csvNameFin = csvName.toArray(new String[0]);
         String[] csvQTYFin = csvQTY.toArray(new String[0]);
@@ -48,45 +47,77 @@ public class test{
     }
 
 
+    // Compares the data from two CSV files and produces a new CSV file with the results
     public static void outputCSV(List<String[]> file1, List<String[]> file2) {
         String filePath = "testOutput.csv";
         List<String[]> csv1 = csvData(file1);
         List<String[]> csv2 = csvData(file2);
-        int maxArrayLength = 0;
+        List<String[]> largerCSV = null; 
+        List<String[]> smallerCSV = null;
+
+        // Determine which CSV file has more data in order to not get out of bounds error
         if(csv1.get(0).length > csv2.get(0).length ) {
-            maxArrayLength = csv1.get(0).length;
+            largerCSV = csv1;
+            smallerCSV = csv2;
         }
         else {
-            maxArrayLength = csv2.get(0).length;
+            largerCSV = csv2;
+            smallerCSV = csv1;
         }
+
+        // Lists to store the locations of matching and non-matching IDs
         List<Integer> commonID1 = new ArrayList<>();
         List<Integer> commonID2 = new ArrayList<>();
-
+        List<Integer> uncommonID = new ArrayList<>();
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
 
+            // Write header for output CSV file
             writer.write("ID,Name,QTY1,QTY2,Difference");
             writer.newLine();
             
-            for (int i = 0; i < csv1.get(0).length; i++) {
-                for(int j = 0; j < csv2.get(0).length; j++) {
-                    if (csv1.get(0)[i].equalsIgnoreCase(csv2.get(0)[j])) {
-
+            for (int i = 0; i < largerCSV.get(0).length; i++) {
+                for(int j = 0; j < smallerCSV.get(0).length; j++) {
+                    if (largerCSV.get(0)[i].equalsIgnoreCase(smallerCSV.get(0)[j])) {
                         commonID1.add(i);
                         commonID2.add(j);
+                    }  
+                    else if (!largerCSV.get(0)[i].equalsIgnoreCase(smallerCSV.get(0)[j]) && check(uncommonID, i)) {
+                        uncommonID.add(i);
                     }
                 }
             }
-    
+
+            // Checks which indexes did not have a match and adds them to a new array
+            for (int i = 0; i < largerCSV.get(0).length; i++) {
+                if (check(commonID1, i)){
+                    uncommonID.add(i); 
+                }
+            }
+     
+
+            // Write data for matched IDs to the output CSV file
             for (int i = 0; i < commonID1.size(); i++) {
-                String id = csv1.get(0)[commonID1.get(i)];
-                String name = csv1.get(1)[commonID1.get(i)];
-                String qty1 = csv1.get(2)[commonID1.get(i)];
-                String qty2 = csv2.get(2)[commonID2.get(i)];
+                String id = largerCSV.get(0)[commonID1.get(i)];
+                String name = largerCSV.get(1)[commonID1.get(i)];
+                String qty1 = largerCSV.get(2)[commonID1.get(i)];
+                String qty2 = smallerCSV.get(2)[commonID2.get(i)];
                 writer.write(id + "," + name + "," + qty1 + "," + qty2 + "," + !qty1.equalsIgnoreCase(qty2));
                 writer.newLine();
             }
-        }
+
+
+          // Write data for unmatched IDs to the output CSV file
+            for (int i = 0; i < uncommonID.size(); i++) {
+                String id = largerCSV.get(0)[uncommonID.get(i)];
+                String name = largerCSV.get(1)[uncommonID.get(i)];
+                String qty1 = largerCSV.get(2)[uncommonID.get(i)];
+                String qty2 = "0";
+                writer.write(id + "," + name + "," + qty1 + "," + qty2 + "," + "true");
+                writer.newLine();
+                }
+            }
+            
         
     catch (IOException e) {
         e.printStackTrace();
@@ -94,6 +125,7 @@ public class test{
 }
 
 
+ // Method to read data from a CSV file and return as a list of string arrays
     public static List<String[]> readCSV(String filePath) throws IOException{
         List<String[]> csvData = new ArrayList<>();
         try(BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
@@ -107,5 +139,19 @@ public class test{
     }
 
 
-    
+
+    // Method to check if an index exists in a list
+    public static boolean check(List<Integer> arrayList, int i) {
+        boolean bool = false;
+        if (arrayList.size() == 0) {
+            return bool;
+        }
+        else {
+            if (!arrayList.contains(i)) {
+                bool = true;
+        }
+        return bool; 
+    }
+
+}
 }
